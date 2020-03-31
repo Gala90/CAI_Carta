1c9ad5a67b886d5291f17517e828e30c17fccae7
Gala <galamagdalena@hotmail.com>
2020-03-31 18:45:39 -0300

    conThrowTryCatch

---
+++ Carta/Carta/Carta.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carta
+{
+    public class Carta
+    {
+        private string tipo;
+        private string valor;
+        private string palo;
+        private List<String> juegosPosibles;
+
+        
+        public Carta (string tipo, string valor, string palo)
+        {
+            this.tipo = tipo;
+            this.valor = valor;
+            this.palo = palo;
+            juegosPosibles = new List<string>();
+        }
+
+        public String Tipo
+        {
+            get
+            {
+                return this.tipo;
+            }
+            set { this.tipo = value; }
+        }
+
+        public String Valor
+        {
+            get
+            {
+                return this.valor;
+            }
+            set { this.valor = value; }
+        }
+
+        public String Palo
+        {
+            get
+            {
+                return this.palo;
+            }
+            set { this.palo = value; }
+        }
+
+        public void mostrarCarta()
+        {
+            List<string> valoresPosibles = new List<string> { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "11", "12", "10", "A", "J", "Q", "K" };
+            if (!valoresPosibles.Contains(Valor))
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            else
+            {
+                Console.WriteLine("Tipo: " + Tipo + " Valor: " + Valor + " Palo: " + Palo);
+            }
+        }
+
+        public void agregarJuego(string juego)
+        {
+            juegosPosibles.Add(juego);
+        }
+
+        public void mostrarJuegos()
+        {   
+            if (juegosPosibles == null)
+            {
+                throw new NullReferenceException("No puede ser null");
+            }
+                
+            if (juegosPosibles.Count < 0)
+            {
+                Console.WriteLine("No hay juegos cargados posibles.");
+
+            } else
+            {
+                foreach (string value in juegosPosibles)
+                {
+                    Console.WriteLine(value);
+                }
+            }
+        }
+
+
+        }
+
+}
+
---
+++ Carta/Carta/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carta miCarta1 = new Carta("Española", "8", "Oro");
+
+            Carta miCarta2 = new Carta("Española", "18", "Oro");
+
+            miCarta1.mostrarCarta();
+         
+            try
+            {
+                miCarta2.mostrarCarta();
+            }
+            catch (ArgumentOutOfRangeException )
+            {
+                Console.WriteLine("El valor " + miCarta2.Valor + " no es posible");
+
+            }
+
+            try
+            {
+                miCarta1.mostrarJuegos();
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("No puede ser null el listado");   //Esto funciona si no inicializo la lista en Carta.cs
+
+            }
+
+
+            string juego;
+            Console.WriteLine("Que juego quiere agregar? ");
+            juego = Console.ReadLine();
+            miCarta1.agregarJuego(juego);
+            miCarta1.mostrarJuegos();
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
